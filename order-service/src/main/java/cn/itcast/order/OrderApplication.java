bb42710f4b7c260848db09da67a2826c7ab61193
yuexia <againstfate@foxmail.com>
2022-07-06 15:09:42 +0800

    day2

---
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -0,0 +1,36 @@
+package cn.itcast.order;
+
+import com.alibaba.cloud.nacos.ribbon.NacosRule;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@MapperScan("cn.itcast.order.mapper")
+@SpringBootApplication
+@EnableFeignClients
+@RefreshScope
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+//    @Bean
+//    @LoadBalanced
+//    public RestTemplate restTemplate(){
+//        return new RestTemplate();
+//    }
+//
+//    @Bean
+//    public IRule rule(){
+//        return new NacosRule();
+//    }
+
+}
